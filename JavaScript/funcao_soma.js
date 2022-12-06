ef8ff31ac794e0fd29b45f838a89243a634ebc01
pbcarvalho <peterson.carvalho20@gmail.com>
2022-12-06 19:09:19 -0300

    primeiros exercicios de Js

---
+++ JavaScript/funcao_soma.js
@@ -0,0 +1,9 @@
+var a = 3;
+var b = 4;
+
+function soma(a, b) {
+    var soma = a + b;
+    return soma;
+}
+
+console.log(soma (a, b));
---
+++ JavaScript/hello_world_fincao.js
@@ -0,0 +1,7 @@
+var texto = "Hello World";
+
+function imprimirTexto(textoParaImprimir) {
+    console.log(textoParaImprimir);
+}
+
+imprimirTexto(texto);
