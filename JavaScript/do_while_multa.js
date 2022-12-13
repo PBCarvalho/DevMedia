b3828ee884a8edc1ca7da75b456f6cf6a84b0be1
pbcarvalho <peterson.carvalho20@gmail.com>
2022-12-13 18:39:32 -0300

    exercicios JS

---
+++ JavaScript/do_while_multa.js
@@ -0,0 +1,17 @@
+let valor_mensalidade = 100;
+let taxa_multa = 0.02;
+let total_dias = 10;
+let contador = 1;
+
+do{
+
+    let dias_atraso = contador;
+
+    let valor_multa = valor_mensalidade * taxa_multa * dias_atraso;
+
+    let nova_mensalidade = valor_mensalidade + valor_multa;
+
+    console.log("Dia(s) de atraso: " + dias_atraso + " - mensalidade atualizada: R$ " + nova_mensalidade + ",00");
+
+    contador++
+} while(contador < total_dias);
