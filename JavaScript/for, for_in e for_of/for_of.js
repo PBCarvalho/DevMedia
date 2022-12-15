f917c193727fb2a5612400b23d0e159b4e56537f
pbcarvalho <peterson.carvalho20@gmail.com>
2022-12-15 11:00:21 -0300

    organizacao de pastas

---
+++ JavaScript/for, for_in e for_of/for_of.js
@@ -0,0 +1,12 @@
+let emails_alunos = [
+    "danilo@email.com",
+    "jessica@email.com",
+    "luana@email.com",
+    "gustavo@email.com"
+];
+
+for ( let email of emails_alunos ) {
+
+    console.log("Email: " + email);
+
+};
---
+++ JavaScript/for, for_in e for_of/for_of_novo_preco.js
@@ -0,0 +1,19 @@
+let produto_array = [
+    { id: 1, nome: "mouse", preco: 50 },
+    { id: 2, nome: "teclado", preco: 90 },
+    { id: 3, nome: "monitor", preco: 350 },
+    { id: 4, nome: "nobreak", preco: 600 },
+];
+
+let percentual_desconto = 10;
+
+for(let produto of produto_array){
+
+    let nome = produto.nome;
+    let preco = produto.preco;
+
+    let valor_desconto = (preco * percentual_desconto) / 100;
+    let novo_preco = preco - valor_desconto;
+
+    console.log("Novo valor do produto " + nome + ": R$" + novo_preco + ",00");
+}
