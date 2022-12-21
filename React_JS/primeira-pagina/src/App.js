5e9abda399e7d7932dca4182992c6f809ad1e224
pbcarvalho <peterson.carvalho20@gmail.com>
2022-12-20 21:17:29 -0300

    exercício React JS

--- React_JS/primeira-pagina/src/App.js
+++ React_JS/primeira-pagina/src/App.js
@@ -1,7 +1,11 @@
+import './App.css';
+
 function App() {
   return (
     <div className="container">
       <h1>Hello world</h1>
+      <p className="tecnologia">Com React JS</p>
+      <p className="autor">Criado por Peterson Carvalho</p>
     </div>
   );
 }
