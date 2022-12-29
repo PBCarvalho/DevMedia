2fedfc9b3d7ce08a6fe6fedca753b7cd6d43772f
pbcarvalho <peterson.carvalho20@gmail.com>
2022-12-29 16:34:16 -0300

    exercicios JS

--- JavaScript/funcao/teste2.js
+++ JavaScript/funcao/teste2.js
@@ -11,8 +11,3 @@ function retorna_status_aluno(nota_1, nota_2) {
 
     return status;
 }
-
-let status_aluno_1 = retorna_status_aluno(7, 8);
-console.log("Aluno 1: " + status_aluno_1);
-let status_aluno_2 = retorna_status_aluno(4, 7);
-console.log("Aluno 2: " + status_aluno_2);
