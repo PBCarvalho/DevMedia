eaca4e9377d258d0a06e5f68c52623781124de22
pbcarvalho <peterson.carvalho20@gmail.com>
2022-12-31 15:52:52 -0300

    exercicios JS Array

---
+++ JavaScript/array/introducao.js
@@ -0,0 +1,11 @@
+const diasDaSemana = [
+    "domingo",
+    "segunda-feira",
+    "terça-feira",
+    "quarta-feira",
+    "quinta-feira",
+    "sexta-feira"
+];
+
+diasDaSemana.push("sabado");
+diasDaSemana.map((dia)=> console.log(dia));
