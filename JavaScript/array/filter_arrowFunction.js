71fd40090b12832b62e4564eaac50e781a8b5e7f
pbcarvalho <peterson.carvalho20@gmail.com>
2023-01-01 14:56:20 -0300

    exercícios JS filter com arrow function

---
+++ JavaScript/array/filter_arrowFunction.js
@@ -0,0 +1,10 @@
+const carro = [
+    { marca: 'Fiat', modelo: 'Uno', anoFabricacao: 2015 },
+    { marca: 'GM', modelo: 'Onix', anoFabricacao: 2018 },
+    { marca: 'Ford', modelo: 'KA+', anoFabricacao: 2018 },
+    { marca: 'Fiat', modelo: 'Cronos', anoFabricacao: 2020 }
+];
+
+const carrosFiat = carro.filter((carro) => carro.marca == 'Fiat');
+
+console.log(carrosFiat);
