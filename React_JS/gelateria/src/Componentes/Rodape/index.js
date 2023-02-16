b9ee900c2580e26eab31f54fbb1f5fa02c2ff05c
Peterson Carvalho <peterson.carvalho20@gmail.com>
2023-02-16 20:14:35 -0300

    adicionando novos elementos

--- React_JS/gelateria/src/Componentes/Rodape/index.js
+++ React_JS/gelateria/src/Componentes/Rodape/index.js
@@ -1,9 +1,33 @@
 import React from "react";
 
 const Rodape = () => (
-    <div>
+    <footer>
+        <div>
+            <img src="./assets/logo.png" />
 
-    </div>
+            <div>
+                <h3>ENDEREÇO</h3>
+                <p>Av. Bernardino de Campos, 98</p>
+                <p>São Paulo, SP 12345-678</p>
+            </div>
+
+            <div>
+                <h3>CONTATO</h3>
+                <p>info@meusite.com</p>
+                <p>Tel: (11) 3456-7890</p>
+            </div>
+
+            <div>
+                <h3>HORÁRIOS</h3>
+                <p>ABERTO TODOS OS DIAS</p>
+                <p>10:00 - 20:00</p>
+            </div>
+        </div>
+
+        <div>
+            <p>Gelateria. Orgulhosamente desenvolvido por "Peterson Carvalho"</p>
+        </div>
+    </footer>
 );
 
 export default Rodape;
--- React_JS/gelateria/src/paginas/Home/index.js
+++ React_JS/gelateria/src/paginas/Home/index.js
@@ -14,7 +14,7 @@ export default function Home() {
                     </div>
                 </section>
 
-                <section className='sabores'>
+                <section>
                     <img src='./assets/banner-sabores.jpg' alt="sorvetes diversos" />
                     <div>
                         <h2>NOSSOS SABORES</h2>
@@ -30,11 +30,31 @@ export default function Home() {
                 </section>
 
                 <section>
-
+                    <div>
+                        <h2>NOSSOS EVENTOS</h2>
+                        <span>Delícias com sorvete!</span>
+                        <p>
+                            Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui
+                            prontinhos para te atender e oferecer os melhores eventos com os melhores
+                            sorvete da sua vida! Venha nos conhecer e passar um tempo aqui com a gente.
+                        </p>
+                    </div>
+                    <img src='./assets/eventos-image.jpg' alt='mesa de festa' />
                 </section>
 
                 <section>
-
+                    <img src='./assets/sobre-image.jpg' alt='pessoas comendo sorvete' />
+                    <div>
+                        <h2>SOBRE NÓS</h2>
+                        <span>Alegria em cada casquinha!</span>
+                        <p>
+                            Venha tomar o melhor sorvete da região aqui com a gente! Nós
+                            estamos há anos no mercado produzindo o que tem de melhor para o
+                            nosso cliente e você não pode ficar fora dessa. Venha nos fazer
+                            uma visita e aproveite o melhor atendimento e o melhor sorvete
+                            da cidade.
+                        </p>
+                    </div>
                 </section>
             </main>
 
