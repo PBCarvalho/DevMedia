73932528ecc68df80d2d30db8f563d4a44fc760f
Peterson Carvalho <peterson.carvalho20@gmail.com>
2023-02-21 12:38:53 -0300

    aprendendo modificação de comportamento de pagina com state hook e evento onClick

---
+++ React_JS/modifica-elemento/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [novoTexto, setNovoTexto] = useState()
+
+  function alterarTexto() {
+    setNovoTexto('Hello, World!')
+  }
+
+  return (
+    <div className='App'>
+      <div className="conteudo-centralizado">
+        <div className='area-texto'>
+          <h2> {novoTexto} </h2>
+        </div>
+        <div className='area-botao'>
+          <label>
+            Click no botão para aparecer um texto:
+          </label>
+          <button onClick={alterarTexto}>
+            Gerar texto
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
