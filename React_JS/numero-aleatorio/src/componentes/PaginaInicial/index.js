1ad909b8ea454036a39afd24550fa40c3050378d
Peterson Carvalho <peterson.carvalho20@gmail.com>
2023-02-21 08:54:47 -0300

    aprendendo state hook e onclick

--- React_JS/numero-aleatorio/src/componentes/PaginaInicial/index.js
+++ React_JS/numero-aleatorio/src/componentes/PaginaInicial/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './estilo.css';
 
 export default function PaginaInicial() {
+    const [ numeroAleatorio, setNumeroAleatorio ] = useState(1);
+
+    function gerarNumero() {
+        const novoNumero = Math.floor(Math.random() * (100-1) + 1);
+        setNumeroAleatorio(novoNumero);
+    }
+
     return (
         <div className='conteudo-centralizado'>
             <h3>Número aleatório:</h3>
-            <h1>0</h1>
+            <h1>{ numeroAleatorio }</h1>
 
             <div className='area-botao'>
                 <label>
                     Click no botão abaixo para gerar um número aleatório:
                 </label>
 
-                <button>
+                <button onClick={ gerarNumero }>
                     Gerar número
                 </button>
             </div>
