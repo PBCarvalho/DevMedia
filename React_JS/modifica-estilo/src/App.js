df84a878a149dda99e00f2b6f91fbf637f206bff
Peterson Carvalho <peterson.carvalho20@gmail.com>
2023-02-21 19:45:52 -0300

    aprendendo modificação de comportamento de pagina com state hook e evento onClick

--- React_JS/modifica-estilo/src/App.js
+++ React_JS/modifica-estilo/src/App.js
@@ -2,21 +2,21 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [novaCor, setNovaCor] = useState('bg-vermelho');
 
+  const corDeFundoVermelho = 'bg-vermelho'
+  const corDeFundoAzul = 'bg-azul'
+  
+  const [estiloAtualizado, setEstiloAtualizado] = useState(false)
+  
   const alterarCor = () => {
-    if(novaCor === 'bg-vermelho') {
-      setNovaCor('bg-azul');
-    } else {
-      setNovaCor('bg-vermelho');
-    }
-  }
-
+    setEstiloAtualizado(!estiloAtualizado)
+  };  
+  
   return (
     <div className="display">
       <div className='container'>
         <h2>Alterando estilo do elemento</h2>
-        <div className={novaCor}></div>
+        <div className={estiloAtualizado ? corDeFundoAzul : corDeFundoVermelho}></div>
         <p>Clique no botão para alterar o estilo do elemento acima.</p>
         <button onClick={alterarCor}>Alterar</button>
       </div>
