eb9a56aa9bf6b6b69aec8cb6d9325805a303e75b
Peterson Carvalho <peterson.carvalho20@gmail.com>
2023-03-19 14:25:47 -0300

    exercicio tipo e variável

---
+++ JavaScript/exercicio.js
@@ -0,0 +1,20 @@
+// 1. Crie uma variável para armazenar o nome
+// do carro e ponha o valor Ferrari e exiba-a.
+
+let nomeDoCarro = "Ferrari"
+console.log("Carro: " + nomeDoCarro)
+console.log("Tipo: " + typeof nomeDoCarro)
+
+// 2. Crie uma variável para armazenar o preço
+// do bolo, que custa R$ 19,99 e exiba-a.
+
+let valorDoBolo = 19.99
+console.log("Valor do bolo R$ " + valorDoBolo)
+console.log("Tipo: " + typeof valorDoBolo)
+
+// 3. Corrija o código a seguir (e exiba-o)
+// let "cidade" = São Paulo
+
+let nomeDaCidade = "São Paulo"
+console.log("Cidade: " + nomeDaCidade)
+console.log("Tipo: " + typeof nomeDaCidade)
